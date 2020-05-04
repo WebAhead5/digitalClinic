859b170a6ee9d99e3308b1524f2c7a4df4299088
moris <moris.rafol@gmail.com>
2020-05-04 19:21:43 +0300

    fix: db models async implementation and rename variables

--- src/models/answers.model.js
+++ src/models/answers.model.js
@@ -1,29 +1,32 @@
 const dbConnection = require('../database/db_connection');
 const validator = require('./validator');
-function getAll() {
-  return async () => {
+
+async function getAll() {
+
     let res = await dbConnection.query('SELECT * FROM answers');
     return res.rows;
   };
-}
 
-function getFor(question_id) {
-  return async () => {
+
+async function getFor(question_id) {
+
     let res = await dbConnection.query(
       'SELECT * FROM answers WHERE question_id = $1',
       [question_id]
     );
     return res.rows;
-  };
+
 }
 
-function add(user_id, question_id, context) {
+async function add(user_id, question_id, context) {
+
   if (validator.isEmptyString(context))
     throw new Error('message context cannot be empty');
 
-  return  dbConnection.query(
+   await  dbConnection.query(
       `INSERT INTO answers (user_id,question_id,answer_context) VALUES ($1, $2 , $3)`,
       [user_id, question_id, context]);
+
 
 }
 
