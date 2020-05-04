e819b7daf96f5552071b40cb5ce1c2907d977457
Morad Abed <morad890@gmail.com>
2020-05-04 13:49:43 +0300

    adding the tables to the db_build.sql

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -7,16 +7,43 @@ BEGIN;
 -- ALTER DATABASE project_db OWNER TO super;
 
 
---DROP TABLE IF EXISTS sitters,reservations CASCADE;
+DROP TABLE IF EXISTS users,questions, answers CASCADE;
 
 
---CREATE TABLE table_1(
---                        id SERIAL PRIMARY KEY,
---                        name VARCHAR(100) NOT NULL,
---
---);
+CREATE TABLE users(
+                       user_id SERIAL PRIMARY KEY,
+                       first_name VARCHAR(100) NOT NULL,
+                       last_name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       passsword TEXT NOT NULL,
+                       doctor_certificate VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE questions(
+                       id SERIAL PRIMARY KEY,
+                       asker_id INTEGER,
+                       FOREIGN KEY (asker_id) REFERENCES users (user_id),
+                       question_context TEXT NOT NULL,
+                       post_time TIMESTAMP NOT NULL
+);
 
 
+CREATE TABLE answers(
+                       id SERIAL PRIMARY KEY,
+                       question_id INTEGER,
+                       FOREIGN KEY (question_id) REFERENCES questions (id),
+                       answer_context TEXT NOT NULL,
+                       post_time TIMESTAMP NOT NULL
+);
+
+
+CREATE TABLE sessions(
+                       session_id SERIAL PRIMARY KEY,
+                       user_id INTEGER,
+                       FOREIGN KEY (user_id) REFERENCES users (user_id),
+                       start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       duration_min INTEGER
+);
 
 
 --INSERT INTO sitters (name,starting_hour, end_hour, cost)
