48708436a7c5bf8e19591049b69dbbf234fa31dc
moris <moris.rafol@gmail.com>
2020-05-04 15:17:00 +0300

    fix: validator.js tweaks

--- src/models/validator.js
+++ src/models/validator.js
@@ -1,4 +1,4 @@
-function isLettersOnly(str) {
+function isLettersAndSpaces(str) {
   const regex = /[a-z ]/gi;
   return str && str.trim() && regex.test(str);
 }
@@ -14,35 +14,35 @@ function validatePassword(str) {
 
   // check for whitespace characters----------------
   const regex_spaces = /\s+/g;
-  isValid = !regex_spaces.test(input);
+  isValid = !regex_spaces.test(str);
   if (!isValid) {
     errorMessage = 'No spaces are allowed in the password';
     return { isValid, errorMessage, strength };
   }
 
   //check if password is weak-----------------------
-  if (input.length < 6) {
+  if (str.length < 6) {
     isValid = false;
     errorMessage = 'the password must contain at least 6 characters';
     return { isValid, errorMessage, strength };
   } else strength++;
 
-  if (input.length > 10) strength++;
+  if (str.length > 10) strength++;
 
   //contains a small letter followed by a capital letter
   // or a capital letter followed by a small letter
   let regex = /([a-z]+.*[A-Z]+)|([A-Z]+.*[a-z]+)/g;
-  if (regex.test(input)) strength++;
+  if (regex.test(str)) strength++;
 
   //contains a number
-  regex = /\d+/g;
-  if (regex.test(input)) strength++;
+  regex = /\d/g;
+  if (regex.test(str)) strength++;
 
   //contains a special char
   regex = /[^(a-z0-9)]/gi;
-  if (regex.test(input)) strength++;
+  if (regex.test(str)) strength++;
 
   return { isValid, errorMessage, strength, total: 5 };
 }
 
-module.exports = { isEmptyString, isLettersOnly, validatePassword };
+module.exports = { isEmptyString, isLettersAndSpaces, validatePassword };
