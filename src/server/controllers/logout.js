30272ef225858e9e280cb811d157d1484e784cd3
moris <moris.rafol@gmail.com>
2020-05-05 19:34:29 +0300

    fix: user can log in and log out

--- src/server/controllers/logout.js
+++ src/server/controllers/logout.js
@@ -2,9 +2,9 @@ const path = require('path');
 const session = require('../../models/sessions.model');
 
 exports.get = (req, res) => {
-  if (res.locals.user && res.local.user.sessionID) {
+  if (res.locals.user) {
     session.delete(res.locals.user.sessionID);
-    res.clearCookies('user_token');
   }
+  res.clearCookie('user_token');
   res.redirect('/');
 };
--- src/server/middleware/validateCookie.js
+++ src/server/middleware/validateCookie.js
@@ -4,9 +4,12 @@ const jwt = require("jsonwebtoken")
 
 async function validateCredentials(req,res,next) {
 
+
+  //  return next();
+
     let token;
 
-    if(req.cookies)
+    if(req.cookies && req.cookies.user_token)
         token =req.cookies.user_token;
 
     if(token) {
@@ -14,31 +17,35 @@ async function validateCredentials(req,res,next) {
         let jwtSecret = process.env.JWT_SECRET;
 
         if(!jwtSecret)
-            next(Error("JWT_SECRET is not set as an environment variable"));
+           return  next(Error("JWT_SECRET is not set as an environment variable"));
 
 
         let decoded;
 
         try {
-            decoded = jwt.verify(token, jwtSecret)
+            decoded =  jwt.verify(token, jwtSecret)
         } catch (e) {
             res.clearCookie("user_token");
             res.locals.user = undefined;
         }
 
-        if(await sessionModule.isExpired(decoded))
-            res.redirect("/logout")
-
-        let sessionData = await sessionModule.getBySessionId(decoded)
-        res.locals.user = {
-            firstName: sessionData.first_name,
-            lastName:  sessionData.last_name,
-            email:  sessionData.email,
-            id:  sessionData.user_id,
-            isDoctor:  doctor_certificate,
-            sessionID: decoded
+        try {
+            let sessionData = await sessionModule.getBySessionId(decoded.session_id)
+            res.locals.user = {
+                firstName: sessionData.first_name,
+                lastName: sessionData.last_name,
+                email: sessionData.email,
+                id: sessionData.user_id,
+                doctorCer: sessionData.doctor_certificate,
+                sessionID: decoded.session_id
+            }
+        } catch (e) {
+            next(e)
         }
 
+
+        if(await sessionModule.isExpired(decoded.session_id))
+            return res.redirect("/logout")
 
     }
     next();
