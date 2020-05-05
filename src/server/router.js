83d89f6ab629723615abcdd0cecfdae804b09233
moris <moris.rafol@gmail.com>
2020-05-05 19:29:52 +0300

    feat: add cookie-parser npm dependency

--- src/server/router.js
+++ src/server/router.js
@@ -5,20 +5,21 @@ const homeRoute = require("./controllers/homeRoute");
 const login = require('./controllers/login');
 const registerRoute = require('./controllers/registerRoute')
 const error = require('./controllers/error');
-const logOut = require('./controllers/logout');
+const logout = require('./controllers/logout');
 const questions = require("./controllers/questions")
 const dashBoard = require("./controllers/dashboard")
 
 const validateLogin = require("./middleware/validateCookie")
 const createSession = require("./middleware/createSession")
 
+
 router.use(validateLogin);
 router.get("/",homeRoute.get);
 router.route('/login').get(login.get).post(login.post,createSession);
 router.route('/register').get(registerRoute.get).post(registerRoute.post,createSession)
-router.get('/logout', logOut.get);
 router.get('/questions',questions.get);
 router.route("/dashboard").get(dashBoard.get).post(dashBoard.post)
+router.get('/logout', logout.get);
 
 
 //error handling
