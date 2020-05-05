b86dee946ad98c9878a737040142786f76ff91fe
moris <moris.rafol@gmail.com>
2020-05-05 21:35:01 +0300

    refactor: createSession.js and add comments

--- src/server/middleware/createSession.js
+++ src/server/middleware/createSession.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken")
 
 async function createSession  (req,res,next){
 
+    //--------------------------------------------------------------------------------------------------
+    //send user to 502 if environment variables are not set
     let sessionDuration = process.env.SESSION_DURATION_MINS;
     let jwtSecret = process.env.JWT_SECRET;
 
@@ -12,13 +14,23 @@ async function createSession  (req,res,next){
     if(!jwtSecret)
         next(new Error("JWT_SECRET is not set as an environment variable"));
 
-    if(res.locals && res.locals.loginUserID) {
-        let session = await sessionModule.add(res.locals.loginUserID, sessionDuration);
-        let sessionID = session.session_id;
-        res.cookie ("user_token" ,jwt.sign({session_id:sessionID},jwtSecret))
-       return  res.redirect("/dashboard")
-    }
-    next(new Error("an error accrued while creating the user session"))
+    //--------------------------------------------------------------------------------------------------
+
+    //if the user hasn't logged in - if the middleware wasn't provided with a user id
+    if(!res.locals || !res.locals.loginUserID)
+        next(new Error("an error accrued while creating the user session - res.locals.loginUserID was not provided "))
+
+
+    // create session
+    let session = await sessionModule.add(res.locals.loginUserID, sessionDuration);
+    let sessionID = session.session_id;
+
+    //set the session id
+    res.cookie ("user_token" ,jwt.sign({session_id:sessionID},jwtSecret))
+
+    //redirect the user to /dashboard after a successful log in
+    return  res.redirect("/dashboard")
+
 }
 
 module.exports = createSession;
