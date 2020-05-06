201ef4a17840aeb0e30384d0bccfac3c2b11d2e9
Morad Abed <morad890@gmail.com>
2020-05-06 16:47:22 +0300

    initial

---
+++ public/scripts/dom/register.dom.js
@@ -0,0 +1,20 @@
+// const firstNameField = document.querySelector("[name = 'firstName']");
+// const lastNameField = document.getElementsByName('lastName')[0];
+
+// function validatingText(str, target) {
+//     if (typeof str !== string || str.length == 0) {
+//         target.addEventListener('onchange', () => {
+//             document.querySelector('regErr').innerText = error;
+//         })
+//     }
+// }
+// console.log(firstNameField)
+// console.log('grrrrr')
+
+// firstNameField.oninput = () => {
+//     document.querySelector('regErr').value = 'Name must be a string'
+// }
+
+// validatingText = () =>{
+// }
+// validatingText()
