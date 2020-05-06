78ac684c388c812850bbd3405e432d1723cf3609
Morad Abed <morad890@gmail.com>
2020-05-06 23:26:11 +0300

    adding front end validations

--- public/scripts/dom/register.dom.js
+++ public/scripts/dom/register.dom.js
@@ -1,20 +1,77 @@
-// const firstNameField = document.querySelector("[name = 'firstName']");
-// const lastNameField = document.getElementsByName('lastName')[0];
+const theForm = document.getElementById('regForm');
 
-// function validatingText(str, target) {
-//     if (typeof str !== string || str.length == 0) {
-//         target.addEventListener('onchange', () => {
-//             document.querySelector('regErr').innerText = error;
-//         })
-//     }
-// }
-// console.log(firstNameField)
-// console.log('grrrrr')
+const firstNameElement = theForm.elements['firstName'];
+const lastNameElement = theForm.elements['lastName'];
+const errText = document.createElement('h1');
 
-// firstNameField.oninput = () => {
-//     document.querySelector('regErr').value = 'Name must be a string'
-// }
 
-// validatingText = () =>{
-// }
-// validatingText()
+//passwords
+const newPassword = document.querySelector("[name = 'password']");
+const confirmPassword = document.querySelector("[name = 'confirmpassword']");
+// const confirmPassword = document.getElementsByName('confirmpassword');
+
+
+errText.textContent = '';
+
+function isLettersAndSpaces(str) {
+    const regex = /^[a-z ]+$/gi;
+    return str && str.trim() === str && regex.test(str);
+}
+
+///////when going back to the initial state it shows the err (because of the space)
+//validating first name
+firstNameElement.addEventListener('input', (event) => {
+
+    if (isLettersAndSpaces(firstNameElement.value)) {
+        errText.textContent = ''
+        firstNameElement.style.border = '2.5px solid green'
+    } else {
+        const errContainer = document.createElement('div');
+        theForm.appendChild(errContainer);
+        errContainer.classList.add('errContainer');
+        errText.textContent = "First Name must be a string";
+        errContainer.appendChild(errText)
+        firstNameElement.style.border = '1.5px solid red';
+    }
+    event.preventDefault();
+})
+
+//validating last name
+lastNameElement.addEventListener('input', (event) => {
+
+    if (isLettersAndSpaces(lastNameElement.value)) {
+        errText.textContent = ''
+        lastNameElement.style.border = '2.5px solid green';
+
+    } else {
+        const errContainer = document.createElement('div');
+        theForm.appendChild(errContainer);
+        errContainer.classList.add('errContainer');
+        errText.textContent = "Last Name must be a string";
+        errContainer.appendChild(errText)
+        lastNameElement.style.border = '1.5px solid red';
+    }
+    event.preventDefault();
+})
+
+
+
+confirmPassword.addEventListener('input', () => {
+ 
+console.log(newPassword.value, confirmPassword.value);
+    if (newPassword.value !== confirmPassword.value) {
+        errText.textContent = 'Passwords do not match';
+        newPassword.style.border = '1.5px solid red';
+        confirmPassword.style.border = '1.5px solid red';
+    } else {
+        newPassword.style.border = '2.5px solid green';
+        confirmPassword.style.border = '2.5px solid green';
+    }
+
+
+event.preventDefault();
+})
+
+    
+    
+
